d95576588363f7c1e71bd1d4631cc1c6a1ed9410
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 23:21:12 +0900

    20230528 #9 EnjuRW02

---
+++ src/main/kotlin/net/konohana/sakuya/phacelia/models/enju/EnjuRW02FareDist.kt
@@ -0,0 +1,25 @@
+package net.konohana.sakuya.phacelia.models.enju
+
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.MappedProperty
+
+/**
+ * ## EnjuRW02FareDistモデル
+ * @author lafleurblanche
+ */
+@MappedEntity(value = "enju_rw02_faredist")
+data class EnjuRW02FareDist(
+    @field:GeneratedValue
+    @field:Id
+    var id: Int,
+    @MappedProperty("sta_code")
+    var staCode: String,
+    @MappedProperty("sta_name")
+    var staName: String,
+    @MappedProperty("strt_pt_sta_code")
+    var strtPtStaCode: String,
+    @MappedProperty("distance")
+    var distance: Double,
+)
