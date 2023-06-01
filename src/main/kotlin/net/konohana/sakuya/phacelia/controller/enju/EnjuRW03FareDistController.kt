3c9070c4ab0dee2f873601bdd3660ab572a887dc
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-01 16:02:10 +0900

    20230601 #34 EnjuRW03

---
+++ src/main/kotlin/net/konohana/sakuya/phacelia/controller/enju/EnjuRW03FareDistController.kt
@@ -0,0 +1,27 @@
+package net.konohana.sakuya.phacelia.controller.enju
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import net.konohana.sakuya.phacelia.models.enju.EnjuRW03FareDist
+import net.konohana.sakuya.phacelia.repositories.enju.EnjuRW03FareDistRepository
+import reactor.core.publisher.Mono
+
+/**
+ * ## EnjuRW03FareDistコントローラ
+ * @author lafleurblanche
+ */
+@Controller("/enjurw03faredist")
+class EnjuRW03FareDistController(
+    private val enjuRW03FareDistRepository: EnjuRW03FareDistRepository
+) {
+    /**
+     * 駅名コードから距離情報を取得します.
+     * @param staCode 駅名コード
+     * @return 距離情報
+     */
+    @Get("/{staCode}")
+    fun findByStaCode(@PathVariable staCode: String): Mono<EnjuRW03FareDist> {
+        return enjuRW03FareDistRepository.findByStaCode(staCode)
+    }
+}
