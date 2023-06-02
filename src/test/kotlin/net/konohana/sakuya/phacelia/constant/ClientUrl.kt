8c4e1cd671d9b708910875e17baf2b59c18e534c
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-03 01:28:48 +0900

    20230603 NEW

---
+++ src/test/kotlin/net/konohana/sakuya/phacelia/constant/ClientUrl.kt
@@ -0,0 +1,14 @@
+package net.konohana.sakuya.phacelia.constant
+
+class ClientUrl {
+    companion object {
+        const val CLIENT_URL_AREAGW07_NOT_EXIST = "/enjurw07faredist/RW9999"
+        const val CLIENT_URL_AREAGW07_EXIST = "/enjurw07faredist/RW0701"
+        const val CLIENT_URL_AREAGW08_NOT_EXIST = "/enjurw08faredist/RW9999"
+        const val CLIENT_URL_AREAGW08_EXIST = "/enjurw08faredist/RW0801"
+        const val CLIENT_URL_AREAGW09_NOT_EXIST = "/enjurw09faredist/RW9999"
+        const val CLIENT_URL_AREAGW09_EXIST = "/enjurw09faredist/RW0901"
+        const val CLIENT_URL_AREAGW10_NOT_EXIST = "/enjurw10faredist/RW9999"
+        const val CLIENT_URL_AREAGW10_EXIST = "/enjurw10faredist/RW1001"
+    }
+}
