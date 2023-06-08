70138387818bd2243ce3a75358584054654af1bb
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-08 22:02:49 +0900

    20230608 Add Url

--- src/test/kotlin/net/konohana/sakuya/phacelia/constant/ClientUrl.kt
+++ src/test/kotlin/net/konohana/sakuya/phacelia/constant/ClientUrl.kt
@@ -11,6 +11,10 @@ class ClientUrl {
         const val CLIENT_URL_ENJURW13_NOT_EXIST = "/enjurw13faredist/RW9999"
         const val CLIENT_URL_ENJURW14_NOT_EXIST = "/enjurw14faredist/RW9999"
         const val CLIENT_URL_ENJURW15_NOT_EXIST = "/enjurw15faredist/RW9999"
+        const val CLIENT_URL_ENJURW16_NOT_EXIST = "/enjurw16faredist/RW9999"
+        const val CLIENT_URL_ENJURW17_NOT_EXIST = "/enjurw17faredist/RW9999"
+        const val CLIENT_URL_ENJURW18_NOT_EXIST = "/enjurw18faredist/RW9999"
+        const val CLIENT_URL_ENJURW19_NOT_EXIST = "/enjurw19faredist/RW9999"
         const val CLIENT_URL_ENJURW07_EXIST = "/enjurw07faredist/RW0701"
         const val CLIENT_URL_ENJURW08_EXIST = "/enjurw08faredist/RW0801"
         const val CLIENT_URL_ENJURW09_EXIST = "/enjurw09faredist/RW0901"
@@ -20,5 +24,9 @@ class ClientUrl {
         const val CLIENT_URL_ENJURW13_EXIST = "/enjurw13faredist/RW1301"
         const val CLIENT_URL_ENJURW14_EXIST = "/enjurw14faredist/RW1401"
         const val CLIENT_URL_ENJURW15_EXIST = "/enjurw15faredist/RW1501"
+        const val CLIENT_URL_ENJURW16_EXIST = "/enjurw16faredist/RW1601"
+        const val CLIENT_URL_ENJURW17_EXIST = "/enjurw17faredist/RW1701"
+        const val CLIENT_URL_ENJURW18_EXIST = "/enjurw18faredist/RW1801"
+        const val CLIENT_URL_ENJURW19_EXIST = "/enjurw19faredist/RW1901"
     }
 }
