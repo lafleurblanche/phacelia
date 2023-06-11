487ee0ba9227c12c042e58931102e74a545f0d8b
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-11 21:12:44 +0900

    20230611 #139 EnjuRW19

---
+++ src/test/kotlin/net/konohana/sakuya/phacelia/application/enju/EnjuRW19FareDistControllerTest.kt
@@ -0,0 +1,32 @@
+package net.konohana.sakuya.phacelia.application.enju
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import net.konohana.sakuya.phacelia.constant.ClientUrl
+import net.konohana.sakuya.phacelia.models.enju.EnjuRW19FareDist
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+@MicronautTest(transactional = false)
+class EnjuRW19FareDistControllerTest(@Client("/") val client: HttpClient) {
+    @Test
+    fun testFindNonExistingStaCodeReturn404() {
+        val thrown = assertThrows<HttpClientResponseException> {
+            client.toBlocking().exchange<Any>(ClientUrl.CLIENT_URL_ENJURW19_NOT_EXIST)
+        }
+
+        Assertions.assertNotNull(thrown.response)
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, thrown.status)
+    }
+
+    @Test
+    fun testEnjuRWEExistingStaCode() {
+        val faredist = client.toBlocking().retrieve(ClientUrl.CLIENT_URL_ENJURW19_EXIST, EnjuRW19FareDist::class.java)
+        Assertions.assertEquals("RW1901", faredist.staCode)
+    }
+
+}
